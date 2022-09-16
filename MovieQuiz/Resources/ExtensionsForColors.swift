4fd8ceeac0dea710efd8437e28a1b4cf36810a70
Арсен Хачуков <hachukov.arsen@gmail.com>
2022-09-16 10:59:48 +0300

    Start commit

---
+++ MovieQuiz/Resources/ExtensionsForColors.swift
@@ -0,0 +1,19 @@
+//
+//  ExtensionsForColors.swift
+//  MovieQuiz
+//
+//  Created by Arsen Hachuk on 15.09.2022.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIColor {
+    static var YPBackground: UIColor { UIColor(named: "YP Background")! }
+    static var YPRed: UIColor {UIColor(named: "YP Red")! }
+    static var YPWhite: UIColor {UIColor(named: "YP White")! }
+    static var YPBlack: UIColor {UIColor(named: "YP Black")! }
+    static var YPGray: UIColor {UIColor(named: "YP Gray")! }
+    static var YPGreen: UIColor {UIColor(named: "YP Green")! }
+}
