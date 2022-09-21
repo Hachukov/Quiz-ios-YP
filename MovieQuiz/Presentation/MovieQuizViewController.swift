d68bb061f17077e600881c12ef5a32c368fcb521
Арсен Хачуков <hachukov.arsen@gmail.com>
2022-09-21 18:25:12 +0300

    to added moc data for questions

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -6,6 +6,55 @@ final class MovieQuizViewController: UIViewController {
         super.viewDidLoad()
     }
 }
+    // MARK: - Question Model
+struct QuizQuestion {
+    let image: String
+    let qustion: String
+    let rating: String
+    let correctAnswer: Bool
+}
+    // MARK: - MOK Data
+let theGodfather = QuizQuestion(image: "The Godfather",
+                                qustion: "Рейтинг этого фильма больше чем 6?",
+                                rating: "9,2" ,
+                                correctAnswer: true)
+let theDarkKnight = QuizQuestion(image: "The Dark Knight",
+                                 qustion: "Рейтинг этого фильма больше чем 6?",
+                                 rating: "9",
+                                 correctAnswer: true)
+let killBill = QuizQuestion(image: "Kill Bill" ,
+                            qustion: "Рейтинг этого фильма больше чем 6?",
+                            rating: "8,1",
+                            correctAnswer: true)
+let theAvengers = QuizQuestion(image: "The Avengers",
+                               qustion: "Рейтинг этого фильма больше чем 6?",
+                               rating: "8",
+                               correctAnswer: true)
+let deadpool = QuizQuestion(image: "Deadpool",
+                            qustion: "Рейтинг этого фильма больше чем 6?",
+                            rating: "8",
+                            correctAnswer: true)
+let theGreenKnight = QuizQuestion(image: "The Green Knight",
+                                  qustion: "Рейтинг этого фильма больше чем 6?",
+                                  rating: "6,6",
+                                  correctAnswer: true)
+let old = QuizQuestion(image: "Old",
+                       qustion: "Рейтинг этого фильма больше чем 6?",
+                       rating: "5,8",
+                       correctAnswer: false)
+let theIceAgeAdventuresofBuckWild = QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
+                                                 qustion: "Рейтинг этого фильма больше чем 6?",
+                                                 rating: "4,3",
+                                                 correctAnswer: false)
+let tesla = QuizQuestion(image: "Tesla",
+                         qustion: "Рейтинг этого фильма больше чем 6?",
+                         rating: "5,1",
+                         correctAnswer: false)
+let vivarium = QuizQuestion(image: "Vivarium",
+                            qustion: "Рейтинг этого фильма больше чем 6?",
+                            rating: "5,8",
+                            correctAnswer: false)
+
 
 /*
  Mock-данные
