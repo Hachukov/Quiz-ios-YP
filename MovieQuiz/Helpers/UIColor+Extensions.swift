14dc533228471daf77c9529767341abb3dc4bc2a
Арсен Хачуков <hachukov.arsen@gmail.com>
2022-09-21 15:46:09 +0300

    changed place for color extensions

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,11 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var YPBackground: UIColor { UIColor(named: "YP Background")! }
+    static var YPRed: UIColor {UIColor(named: "YP Red")! }
+    static var YPWhite: UIColor {UIColor(named: "YP White")! }
+    static var YPBlack: UIColor {UIColor(named: "YP Black")! }
+    static var YPGray: UIColor {UIColor(named: "YP Gray")! }
+    static var YPGreen: UIColor {UIColor(named: "YP Green")! }
+}
