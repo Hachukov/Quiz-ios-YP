6fda122452ca56d653ce2712dda1b79f412f5566
Арсен Хачуков <hachukov.arsen@gmail.com>
2022-09-22 22:29:56 +0300

    готово к ревью

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,59 +2,159 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
+    @IBOutlet weak private var imagView: UIImageView!
+    @IBOutlet weak private var textLabel: UILabel!
+    @IBOutlet weak private var counterLabel: UILabel!
+    private var currentQuestionIndex = 0
+    private var correctAnswers: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        show(quiz: convert(model: questions[currentQuestionIndex]))
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+        self.imagView.image = step.image
+        self.counterLabel.text = step.questionNumber
+    }
+    
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(title: result.title,
+                                      message: result.text,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: result.buttonText,
+                                   style: .default) { _ in
+            // Обнуляем счетчик вопросов
+            self.currentQuestionIndex = 0
+            // Обнуляем счетчик правельных ответов
+            self.correctAnswers = 0
+            self.counterLabel.text = "\(self.correctAnswers + 1)/10"
+            
+            // заново показываем первый вопрос
+            let firstQuestion = questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+        }
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        return QuizStepViewModel(image: UIImage(named: model.image) ?? UIImage(),
+                                 question: model.text,
+                                 questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+    }
+    @IBAction func noButton(_ sender: Any) {
+        if questions[currentQuestionIndex].correctAnswer == false {
+            showAnswerResult(isCorrect: true)
+        } else {
+            showAnswerResult(isCorrect: false)
+        }
+    }
+    
+    @IBAction func yesButton(_ sender: Any) {
+        if questions[currentQuestionIndex].correctAnswer == true {
+            showAnswerResult(isCorrect: true)
+        } else {
+            showAnswerResult(isCorrect: false)
+        }
+    }
+    
+    private func showAnswerResult(isCorrect: Bool) {
+        imagView.layer.masksToBounds = true // разрешение на рисование
+        imagView.layer.borderWidth = 8 // толщина рамки
+        imagView.layer.cornerRadius = 20 // радиус скругление углов
+        
+        if isCorrect {
+            imagView.layer.borderColor = UIColor.YPGreen.cgColor// цвет рамки
+            correctAnswers += 1
+            
+        } else {
+            imagView.layer.borderColor = UIColor.YPRed.cgColor // цвет рамки
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResults()
+            self.imagView.layer.borderWidth = 0
+        }
+    }
+    
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 {
+            let text = "Ваш результат: \(correctAnswers) из 10"
+            let viewModel = QuizResultsViewModel(title: "Раунд окончен",
+                                                 text: text,
+                                                 buttonText: "Сыграть еще раз")
+            show(quiz: viewModel)
+        } else {
+            currentQuestionIndex += 1
+            let nextQuestion = questions[self.currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            self.show(quiz: viewModel)
+        }
     }
 }
-    // MARK: - Question Model
+
+    // MARK: - Models
 struct QuizQuestion {
     let image: String
-    let qustion: String
-    let rating: String
+    let text: String
     let correctAnswer: Bool
 }
-    // MARK: - MOK Data
-let theGodfather = QuizQuestion(image: "The Godfather",
-                                qustion: "Рейтинг этого фильма больше чем 6?",
-                                rating: "9,2" ,
-                                correctAnswer: true)
-let theDarkKnight = QuizQuestion(image: "The Dark Knight",
-                                 qustion: "Рейтинг этого фильма больше чем 6?",
-                                 rating: "9",
-                                 correctAnswer: true)
-let killBill = QuizQuestion(image: "Kill Bill" ,
-                            qustion: "Рейтинг этого фильма больше чем 6?",
-                            rating: "8,1",
-                            correctAnswer: true)
-let theAvengers = QuizQuestion(image: "The Avengers",
-                               qustion: "Рейтинг этого фильма больше чем 6?",
-                               rating: "8",
-                               correctAnswer: true)
-let deadpool = QuizQuestion(image: "Deadpool",
-                            qustion: "Рейтинг этого фильма больше чем 6?",
-                            rating: "8",
-                            correctAnswer: true)
-let theGreenKnight = QuizQuestion(image: "The Green Knight",
-                                  qustion: "Рейтинг этого фильма больше чем 6?",
-                                  rating: "6,6",
-                                  correctAnswer: true)
-let old = QuizQuestion(image: "Old",
-                       qustion: "Рейтинг этого фильма больше чем 6?",
-                       rating: "5,8",
-                       correctAnswer: false)
-let theIceAgeAdventuresofBuckWild = QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
-                                                 qustion: "Рейтинг этого фильма больше чем 6?",
-                                                 rating: "4,3",
-                                                 correctAnswer: false)
-let tesla = QuizQuestion(image: "Tesla",
-                         qustion: "Рейтинг этого фильма больше чем 6?",
-                         rating: "5,1",
-                         correctAnswer: false)
-let vivarium = QuizQuestion(image: "Vivarium",
-                            qustion: "Рейтинг этого фильма больше чем 6?",
-                            rating: "5,8",
-                            correctAnswer: false)
 
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
+
+struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
+    // MARK: - MOK Data
+private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false)
+    ]
 
 /*
  Mock-данные
