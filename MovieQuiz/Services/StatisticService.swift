8d71dfc316ab2875f28101b5d93b73888eafd677
Арсен Хачуков <hachukov.arsen@gmail.com>
2022-10-17 19:56:38 +0300

    модель для сохранения счета

--- MovieQuiz/Services/StatisticService.swift
+++ MovieQuiz/Services/StatisticService.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+protocol StatisticService {
+    var totalAccuracy: Double {get}
+    var gamesCount: Int {get}
+    var bestGame: GameRecord {get}
+}
+
+final class StatisticServiceImplementation: StatisticService {
+    var gamesCount: Int
+    
+    var totalAccuracy: Double = 0.0
+    
+}
+
+struct GameRecord: Codable {
+    let correct: Int
+    let total: Int
+    let date: Date
+}
+
+
